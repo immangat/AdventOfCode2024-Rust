3a1196c6888668426126dbe185711d76aa1835ff
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-12-02 02:14:01 -0800

    add part 1 for day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -12,10 +12,49 @@ fn main() {
 
     let contents = fs::read_to_string("./input.txt").expect("Should read a file here");
     let lines = contents.lines();
-    let reports: Vec<Vec<i64>>= lines.map(|line|{
-        let levels: Vec<_> = line.split_whitespace().collect();
-        levels.iter().filter_map(|number|{
-            number.parse().ok()
-        }).collect()
-    }).collect();
+    let reports: Vec<Vec<i64>> = lines
+        .map(|line| {
+            let levels: Vec<_> = line.split_whitespace().collect();
+            levels
+                .iter()
+                .filter_map(|number| number.parse().ok())
+                .collect()
+        })
+        .collect();
+    let mut count = 0;
+    reports.iter().for_each(|report| {
+        let mut failed = false;
+        let mut first = true;
+        let mut up = true;
+        for i in 0..report.len() - 1 {
+            let difference = report[i] - report[i + 1];
+            if first {  
+                first = false;
+                up = difference < 0;
+            }
+            match difference {
+                0 => {
+                    failed = true;
+                    break;
+                }
+                ..0 => {
+                    if !(difference.abs() <= 3) || !up { 
+                        failed = true;
+                        break;
+                    }
+                    
+                }
+                0.. => {
+                    if !(difference.abs() <= 3) || up {
+                        failed = true;
+                        break;
+                    }
+                }
+            }
+        }
+        if !failed {
+            count += 1
+        }
+    });
+    println!("{count}")
 }

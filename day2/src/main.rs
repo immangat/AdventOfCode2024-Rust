028559d7e9e1d0f30b97926864fb806aabbc7d5b
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-12-02 18:47:25 -0800

    add part 2 for day2. brute force solution

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,16 +1,8 @@
 use std::fs;
+use std::time::Instant;
 
-fn main() {
-    println!("Hello, world!");
-    /*
-    Read the file
-    Read each line
-    Convert each line to vector of numbers
-    Store each in a vector
-
-     */
-
-    let contents = fs::read_to_string("./input.txt").expect("Should read a file here");
+fn part_1(contents: String) {
+    let start = Instant::now();
     let lines = contents.lines();
     let reports: Vec<Vec<i64>> = lines
         .map(|line| {
@@ -28,7 +20,7 @@ fn main() {
         let mut up = true;
         for i in 0..report.len() - 1 {
             let difference = report[i] - report[i + 1];
-            if first {  
+            if first {
                 first = false;
                 up = difference < 0;
             }
@@ -38,11 +30,10 @@ fn main() {
                     break;
                 }
                 ..0 => {
-                    if !(difference.abs() <= 3) || !up { 
+                    if !(difference.abs() <= 3) || !up {
                         failed = true;
                         break;
                     }
-                    
                 }
                 0.. => {
                     if !(difference.abs() <= 3) || up {
@@ -56,5 +47,117 @@ fn main() {
             count += 1
         }
     });
-    println!("{count}")
+    println!("{count}");
+    let duration = start.elapsed();
+
+    // Print the execution time
+    println!("Program executed in: {:?} for part_1", duration);
+}
+
+fn part_2(contents: String) {
+    let start = Instant::now();
+    let lines = contents.lines();
+    let reports: Vec<Vec<i64>> = lines
+        .map(|line| {
+            let levels: Vec<_> = line.split_whitespace().collect();
+            levels
+                .iter()
+                .filter_map(|number| number.parse().ok())
+                .collect()
+        })
+        .collect();
+    let mut count = 0;
+    reports.iter().for_each(|report| {
+        let mut failed = false;
+        let mut first = true;
+        let mut up = true;
+
+        for i in 0..report.len() - 1 {
+            let difference = report[i] - report[i + 1];
+            if first {
+                first = false;
+                up = difference < 0;
+            }
+            match difference {
+                0 => {
+                    failed = true;
+                    break;
+                }
+                ..0 => {
+                    if !(difference.abs() <= 3) || !up {
+                        failed = true;
+                        break;
+                    }
+                }
+                0.. => {
+                    if !(difference.abs() <= 3) || up {
+                        failed = true;
+                        break;
+                    }
+                }
+            }
+        }
+        if !failed {
+            count += 1
+        } else {
+            
+            for j in 0..report.len() {
+                let mut failed = false;
+                let mut first = true;
+                let mut up = true;
+                let mut new_report = report.clone();
+                new_report.remove(j);
+                for i in 0..new_report.len() - 1 {
+                    let difference = new_report[i] - new_report[i + 1];
+                    if first {
+                        first = false;
+                        up = difference < 0;
+                    }
+                    match difference {
+                        0 => {
+                            failed = true;
+                            break;
+                        }
+                        ..0 => {
+                            if !(difference.abs() <= 3) || !up {
+                                failed = true;
+                                break;
+                            }
+                        }
+                        0.. => {
+                            if !(difference.abs() <= 3) || up {
+                                failed = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if !failed {
+                    count += 1;
+                    break
+                }
+            }
+        }
+    });
+    println!("{count}");
+    let duration = start.elapsed();
+
+    // Print the execution time
+    println!("Program executed in: {:?} for part_2", duration);
+}
+fn main() {
+    /*
+    Read the file
+    Read each line
+    Convert each line to vector of numbers
+    Store each in a vector
+    Go over each vector in the vector
+    check if it is safe
+    if yes add to count
+    return count
+     */
+
+    let contents = fs::read_to_string("./input.txt").expect("Should read a file here");
+    part_1(contents.clone());
+    part_2(contents.clone());
 }
